afcfb6dc5d38847a6c6fdb59a59a561c2edd58be
Geoff <webmaster@geoffish.heliohost.org>
2011-05-20 17:50:03 -0500

    Updated JobQueueRepository.

--- Entity/JobQueueRepository.php
+++ Entity/JobQueueRepository.php
@@ -1,3 +1,18 @@
 <?php
 
 namespace Xaav\QueueBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class JobQueueRepository extends EntityRepository
+{
+    public function getJob()
+    {
+
+    }
+
+    public function addJob()
+    {
+
+    }
+}
