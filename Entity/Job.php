110e471347113f56a08a250160e55b2098e05647
Geoff <webmaster@geoffish.heliohost.org>
2011-05-20 17:30:58 -0500

    Added job entity.

--- Entity/Job.php
+++ Entity/Job.php
@@ -1,1 +1,24 @@
 <?php
+
+/**
+ * @orm:Entity
+ */
+class Job
+{
+    /**
+     * @orm:Id
+     * @orm:Column(type="integer")
+     * @orm:GeneratedValue(strategy="AUTO")
+     */
+     protected $id;
+
+    /**
+     * @orm:Column(type="string", length="1000")
+     */
+    protected $name;
+
+    /**
+     * @orm:ManyToOne(targetEntity="JobQueue", inversedBy="Job")
+     */
+    protected $jobQueue;
+}
