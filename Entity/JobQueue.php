7d310e9529e4ac83ec0b719e75612bc3899e18c3
xaav <xaav@xaav.tk>
2011-07-13 17:20:21 -0500

    Fixed DB storage

--- Entity/JobQueue.php
+++ Entity/JobQueue.php
@@ -40,7 +40,7 @@ class JobQueue extends AbstractJobQueue implements JobQueueInterface
 
     protected function getJobs()
     {
-        if($jobs = unserialize($this->jobs)) {
+        if($jobs = base64_decode(unserialize($this->jobs))) {
 
             return $jobs;
         } else {
@@ -51,7 +51,7 @@ class JobQueue extends AbstractJobQueue implements JobQueueInterface
 
     protected function setJobs($jobs)
     {
-        $this->jobs = serialize($jobs);
+        $this->jobs = base64_encode(serialize($jobs));
     }
 
     /**
