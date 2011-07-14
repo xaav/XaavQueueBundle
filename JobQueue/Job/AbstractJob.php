9dfd51f6494894a22fca6d6383b4a4dfdc47d4af
xaav <xaav@xaav.tk>
2011-07-14 13:20:08 -0500

    Updated abstractjob.

--- JobQueue/Job/AbstractJob.php
+++ JobQueue/Job/AbstractJob.php
@@ -8,11 +8,29 @@ namespace Xaav\QueueBundle\JobQueue\Job;
 abstract class AbstractJob implements JobInterface
 {
     protected $count = 0;
+    protected $initialized = false;
+
+    /**
+     * Lazy-loaded startup function.
+     */
+    protected function init()
+    {
+        //Do startup work here
+    }
 
     public function pass()
     {
-        $this->process($this->count);
+        if(!$this->initialized) {
+            $this->init();
+            $this->initialized = true;
+
+            return false;
+        }
+
+        $result = $this->process($this->count);
         $this->count++;
+
+        return $result;
     }
 
     abstract protected function process($count);
